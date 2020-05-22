bfbe1ccc3dd8b83f7b57a969765b0b70d42c71e7
Andres Urizar <andresum1309@gmail.com>
2020-05-22 15:34:12 -0600

    manda que esta listo de nuevo

--- proto_totito.py
+++ proto_totito.py
@@ -12,6 +12,7 @@ class infoGame:
         self.board = []
         self.game_finished = False
         self.player_turn_id = 0
+        self.winner_turn_id = 0
 
 @sio.on('connect')
 def onConnect():
@@ -38,10 +39,15 @@ def ready(server):
     infoGame.game_finished = False
     typeLine = random.randint(0,1)#int(input("0: Horizontal\n 1: Vertical\n"))
     position = random.randint(0,29)#int(input("0-29: "))
+    while int(infoGame.board[typeLine][position]) != 99:
+        typeLine = random.randint(0, 1)
+        position = random.randint(0, 29)
     #while int(infoGame.board[typeLine][position] != 99):
      #   typeLine = int(input("0: Horizontal\n 1: Vertical\n"))
       #  position = int(input("0-29: "))
     movement = [typeLine,position]
+
+    print("Jugada: " + str(movement[0]) + ", " + str(movement[1]))
     
     sio.emit('play',{
         'player_turn_id':infoGame.player_turn_id,
@@ -57,16 +63,21 @@ def reset():
 @sio.on('finish')
 def finish(server):
     reset()
+    infoGame.game_id = server['game_id']
+    infoGame.player_turn_id = server['player_turn_id']
+    infoGame.winner_turn_id = server['winner_turn_id']
 
     if server['player_turn_id'] == server['winner_turn_id']:
         print("Eres el ganador")
     else:
         print("Perdiste")
 
+    infoGame.game_finished = True
+
     sio.emit('player_ready',{
         'tournament_id': infoGame.tournament_id,
         'game_id': infoGame.game_id,
-        'player_turn_id':0
+        'player_turn_id': infoGame.player_turn_id#devolver lo que el arbitro devuelve
     })
 
 
